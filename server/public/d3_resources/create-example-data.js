ec8d5b71816f9cf11893d8788593d6a6bd0759db
Dima <dima.avanesov@yandex.ru>
2019-11-23 21:19:56 +0300

    drawChart works for multiple pieces of shit

--- server/public/d3_resources/create-example-data.js
+++ server/public/d3_resources/create-example-data.js
@@ -5,6 +5,9 @@ function createRandomData(n, range, date, rand) {
     var num = range[0] + Math.floor(Math.random() * (range[1] - range[0]));
     var num2 = range[0] + Math.floor(Math.random() * (range[1] - range[0]));
     var num3 = num;
+    var num4 = num;
+    var num5 = num;
+    var num6 = num;
     var d = new Date(date);
     var data = [];
     var rgen = d3.randomNormal(0, (range[1] - range[0]) * rand);
@@ -14,6 +17,9 @@ function createRandomData(n, range, date, rand) {
         n: num,
         n2: num2,
         n3: num3,
+        n4: num4,
+        n5: num5,
+        n6: num6,
         ci_up: num3 * 1.05,
         ci_down: num3 * 0.95
     });
--- server/public/index.js
+++ server/public/index.js
@@ -9,39 +9,42 @@ async function updateData() {
     let response = await fetch('./data/data.json');
     let parsed = await response.json();
     let csvData = await parsed.data;
-    await drawChart(csvData);
+    await drawChart(csvData, ["f0"], "#chart");
+    await drawChart(csvData, ["f1", "f2", "f3"], "#chart2");
 }
 
-function drawChart(csvData) {
+function drawChart(csvData, attrArray, tagId) {
+    const timeInterval = 1440;
+
     var minValue = 1000000;
     var maxValue = -1000000;
     var rangeCoef = 0.001;
 
-    var currentChart = document.querySelector("#chart");
+    var currentChart = document.querySelector(tagId);
     currentChart.innerHTML = "";
 
-    csvData.forEach(function (item) {
-        if (item.f0 < minValue) {
-            minValue = item.f0;
-        }
-        if (item.f0 > maxValue) {
-            maxValue = item.f0;
-        }
+    attrArray.forEach(function (attrArrayItem) {
+        csvData.forEach(function (csvDataItem) {
+            if (csvDataItem[attrArrayItem] < minValue) {
+                minValue = csvDataItem[attrArrayItem];
+            }
+            if (csvDataItem[attrArrayItem] > maxValue) {
+                maxValue = csvDataItem[attrArrayItem];
+            }
+        });
+        console.log(minValue, maxValue);
     });
-    console.log(minValue, maxValue);
 
     var data = createRandomData(csvData.length, [minValue * (1 - rangeCoef), maxValue * (1 + rangeCoef)], csvData[0].date);
+    var chart = d3_timeseries();
 
-    for (let i = 0; i < csvData.length; i++) {
-        data[i].n = csvData[i].f0;
-    }
+    attrArray.forEach(function (attrArrayItem) {
+        for (let i = 0; i < csvData.length; i++) {
+            data[i][attrArrayItem] = csvData[i][attrArrayItem];
+        }
 
-    var chart = d3_timeseries()
-        .addSerie(data.slice(0, csvData.length), { x: 'date', y: 'n' }, { interpolate: 'linear', color: "#a6cee3", label: "value" })
-        .addSerie(data.slice(0, csvData.length),
-            { x: 'date', y: 'n3', ci_up: 'ci_up', ci_down: 'ci_down' },
-            { interpolate: 'monotone', dashed: true, color: "#a6cee3", label: "prediction" })
-        .width(820);
+        chart.addSerie(data.slice(csvData.length - timeInterval), { x: 'date', y: attrArrayItem }, { interpolate: 'linear', color: "#a6cee3", label: "value" }).width(820);
+    });
 
     chart('#chart');
 }
